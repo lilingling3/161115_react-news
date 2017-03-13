9b0d88c5d2e641efe68043da4b096c22cf6afcfc
zxfjd3g <258147149@qq.com>
2017-03-13 16:19:44 +0800

    update

--- src/index.js
+++ src/index.js
@@ -5,17 +5,28 @@ import App from './components/app'
 import NewsContainer from './components/news_container'
 import NewsDetail from './components/news_detail'
 import UserCenter from './components/user_center'
+import MediaQuery from 'react-responsive'
 
 import './index.css'
 
 render((
-  <Router history={hashHistory}>
-    <Route path='/' component={App}>
-      <IndexRoute component={NewsContainer}></IndexRoute>
-      <Route path='/news_detail/:news_id' component={NewsDetail}></Route>
-      <Route path='/user_center' component={UserCenter}></Route>
-    </Route>
-  </Router>
+
+<div>
+  <MediaQuery query='(min-device-width: 1224px)'>
+    <Router history={hashHistory}>
+      <Route path='/' component={App}>
+        <IndexRoute component={NewsContainer}></IndexRoute>
+        <Route path='/news_detail/:news_id' component={NewsDetail}></Route>
+        <Route path='/user_center' component={UserCenter}></Route>
+      </Route>
+    </Router>
+  </MediaQuery>
+  <MediaQuery query='(max-device-width: 1224px)'>
+    <div>You are a tablet or mobile phone</div>
+  </MediaQuery>
+</div>
+
+
 ), document.getElementById('root'))
 
 
